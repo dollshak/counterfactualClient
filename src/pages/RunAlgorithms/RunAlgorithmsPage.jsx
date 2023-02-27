2f1b2893effe84a197f803e875c1a1bc6afeb7de
dollshak <dollshak@post.bgu.ac.il>
2023-02-27 13:22:23 +0200

    when checked show message

--- src/pages/RunAlgorithms/RunAlgorithmsPage.jsx
+++ src/pages/RunAlgorithms/RunAlgorithmsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const RunAlgorithmsPage = () => {
+  const [showArgs, setShowArgs] = useState(false);
   // const [algorithmsList, setAlgorithmsList] = useState([]);
   const algorithmsList = [
     {
@@ -26,21 +27,39 @@ const RunAlgorithmsPage = () => {
       output_example: ["6000 -> 1200", "4 -> 40"],
     },
   ];
+
+  const onSubmit = () => {};
+
+  const algorithmChecked = (algorithmId) => {
+    var checkBox = document.getElementById(algorithmId);
+    var text = document.getElementById("p" + algorithmId);
+    console.log(checkBox.checked);
+    if (checkBox.checked) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  };
   return (
     <div className="RunAlgorithmsPage">
       <h1>run algorithms</h1>
       <div className="algorithms_list">
         {algorithmsList.map((algorithm) => (
-          <div className="algorithm">
+          <div className="algorithm" key={algorithm._id}>
             <input
               type="checkbox"
               id={algorithm._id}
               name={algorithm.name}
               value={algorithm.name}
+              onClick={() => algorithmChecked(algorithm._id)}
             />
-            <label for={algorithm._id}>{algorithm.name}</label>
+            <label htmlFor={algorithm._id}>{algorithm.name}</label>
+            <div id={"p" + algorithm._id} hidden={true}></div>
           </div>
         ))}
+      </div>
+      <div className="submit button">
+        <button onClick={onSubmit}>run</button>
       </div>
     </div>
   );
