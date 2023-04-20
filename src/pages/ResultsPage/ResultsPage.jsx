5c5e8f9f5bae015f8028cc53fab56eee8f35ddc2
dollshak <dollshak@post.bgu.ac.il>
2023-04-20 15:08:24 +0300

    results table done

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,28 +1,58 @@
 import Table from "./Table/Table";
 import "./ResultsPage.css";
+import { useNavigate } from "react-router-dom";
 
 const ResultsPage = () => {
+  const navigate = useNavigate();
+
   const reqbody = {
     input: { size: 4, color: 2 },
     output: {
-      alibi: [5, 8],
-      dummy: [12, 4],
-      dice: [6, 7],
+      alibi: [
+        [5, 8],
+        [2, 4],
+      ],
+      dummy: [
+        [12, 4],
+        [5, 5],
+        [7, 8],
+      ],
+      dice: [[6, 7]],
     },
   };
 
+  const inputList = Object.entries(reqbody.output).map(([key, value]) => [
+    key,
+    value,
+  ]);
+
+  const body = [["input", [Object.values(reqbody.input)]]];
+
+  const onBackClick = () => {
+    navigate("/");
+  };
+
+  console.log("page input list " + inputList);
+  console.log(Object.values(reqbody.input));
+
   return (
     <div className="resultsPage-container">
-      <p>Results</p>
+      <button className="back_button" onClick={onBackClick}>
+        back
+      </button>
+      <h1>Results</h1>
+      <h2>input</h2>
       <Table
         tableHeaders={Object.keys(reqbody.input)}
-        tableRows={[Object.values(reqbody.input)]}
+        tableRows={body}
+        isInput={true}
       ></Table>
+      <h2>output</h2>
       <Table
-        tableHeaders={Object.keys(reqbody.input)}
-        tableRows={Object.values(reqbody.output)}
+        tableHeaders={["algorithm", ...Object.keys(reqbody.input)]}
+        tableRows={inputList}
+        isInput={false}
       ></Table>
-      {/* <ResultsTable></ResultsTable> */}
     </div>
   );
 };
--- src/pages/ResultsPage/Table/Table.jsx
+++ src/pages/ResultsPage/Table/Table.jsx
@@ -1,9 +1,58 @@
 import React from "react";
+import "./Table.css";
 
-const ResultsTable = ({ tableHeaders, tableRows }) => {
+const ResultsTable = ({ tableHeaders, tableRows, isInput }) => {
+  const reqbody = {
+    input: { size: 4, color: 2 },
+    output: {
+      alibi: [
+        [5, 8],
+        [2, 4],
+      ],
+      dummy: [
+        [12, 4],
+        [5, 5],
+        [7, 8],
+      ],
+      dice: [[6, 7]],
+    },
+  };
+
+  const inputList = [
+    [
+      "alibi",
+      [
+        [5, 8],
+        [2, 4],
+      ],
+    ],
+    [
+      "dummy",
+      [
+        [12, 4],
+        [5, 5],
+        [7, 8],
+      ],
+    ],
+    ["dice", [[6, 7]]],
+  ];
+
+  console.log("inputlist " + inputList);
+
   const headers = tableHeaders.map((columnHeader, index) => (
-    <th>{columnHeader}</th>
+    <th key={index}>{columnHeader}</th>
   ));
+
+  const newRows = tableRows.map((algorithmRowsAndName) =>
+    algorithmRowsAndName[1].map((rowValues, index) => (
+      <tr key={index}>
+        {!isInput && <td>{algorithmRowsAndName[0]}</td>}
+        {rowValues.map((cellValue, index) => (
+          <td key={index}>{cellValue}</td>
+        ))}
+      </tr>
+    ))
+  );
 
   const rows = tableRows.map((rowValues, index) => (
     <tr key={index}>
@@ -19,7 +68,7 @@ const ResultsTable = ({ tableHeaders, tableRows }) => {
         <thead>
           <tr>{headers}</tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody>{newRows}</tbody>
       </table>
     </div>
   );
